863661f8d03a07f2388f63d9a1ef7a43b4e9df5a
parom <E.Parominskiy@isida.by>
2026-04-22 16:01:34 +0300

    50. New project, Class, Method

---
+++ src/test/java/UserServiceTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    void testCreateUser_whenUserDetailsProvided_returnUserObject(){
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+}
